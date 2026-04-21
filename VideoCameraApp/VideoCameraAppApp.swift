fc35992f53b9134518be0cecc9442fb6455571f0
David Emery <Emery2100@yahoo.com>
2026-04-20 20:30:22 -0700

    Initial Commit

---
+++ VideoCameraApp/VideoCameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCameraAppApp.swift
+//  VideoCameraApp
+//
+//  Created by David Emery on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct VideoCameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
